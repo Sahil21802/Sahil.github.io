c5b55465f5a08be77d1c0453fcbce1b482a0b14a
Sahil <ssharma438500>
2023-07-07 14:24:06 +0530

    add index.js

---
+++ js/function.js
@@ -0,0 +1,12 @@
+// FUNCTIONS
+
+//declaring a function 
+function Random(name) {
+    console.log("Function is called !" , name);
+}
+
+Random("sahil");
+
+// function (){
+
+// }
---
+++ js/index.js
@@ -0,0 +1,57 @@
+//creating the object
+
+
+// let user = {
+//   name:"Sahil",
+//   address:{
+//     street:"The Unnamed road",
+//     houseNo:434,
+//   },
+//   phone:12345677889,
+// };
+
+//Note:everything in the javascript is the object
+
+//this will print the user object
+// console.log(user);
+
+//first i have to select the phone field from the "user"object
+
+// second i have to update field with new value
+
+//if you have the change the value of the field your have to use "." and then field name 
+// user.phone = 99999;
+
+//TODO - Add a new field in the object named "isverified" have a value false
+
+// user.isverified = true;
+
+// user.address.houseNo = 438;
+
+// user["isverified"] =true;
+// user.isverified = false;
+// console.log(user);
+
+
+//Creating the array
+
+
+// const user = [];
+// user[0]= "name:Sahil";
+// user[1]= "address: Sector 45";
+// user[2]= "phone:12345678";
+ 
+// user[1] = "sector-34";
+// console.log(user);
+
+// let arr = ["sahil", "Sector-45",435276];
+// console.log(arr.length);
+
+// for(let i = 0;i < arr.length;i++){
+// console.log(arr[i]);
+// }
+
+// arr.forEach(function (item) {
+//     console.log(item);
+// })
+ //
