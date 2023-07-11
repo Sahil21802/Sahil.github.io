755a6affad39adaf289657754beb6a9b61058d2f
Sahil <ssharma438500>
2023-07-11 16:22:35 +0530

    editing

--- js/selector.js
+++ js/selector.js
@@ -1,43 +1,43 @@
-//selecting the element name 
-document.querySelector("nav")
+// //selecting the element name 
+// document.querySelector("nav")
 
-//selecting the element by class
-document.querySelector(".mobile")
+// //selecting the element by class
+// document.querySelector(".mobile")
 
-//selecting the element by id
-document.querySelector("#sidebar")
+// //selecting the element by id
+// document.querySelector("#sidebar")
 
-//selecting multiple elements 
-document.querySelectorAll("div")
+// //selecting multiple elements 
+// document.querySelectorAll("div")
 
-// change the button color
-document.querySelector("[type = 'button']").style.background = "red";
+// // change the button color
+// document.querySelector("[type = 'button']").style.background = "red";
 
-// change the content of element 
-document.querySelector("h1"). innerText = "Updated Value";
-
-
-// click
-document.querySelector("[type='button']").addEventListener("click",function() {
-    console.log("Button is clicked :)");
-});
+// // change the content of element 
+// document.querySelector("h1"). innerText = "Updated Value";
 
 
-// mouseover
-document.querySelector("[type='button']").addEventListener("mouseover",function() {
-    console.log("Button is over :)");
-});
+// // click
+// document.querySelector("[type='button']").addEventListener("click",function() {
+//     console.log("Button is clicked :)");
+// });
 
 
-//whenever button is hover change the h1 content to "Value to change
-document.querySelector("[type='button']").addEventListener("mouseover",function() {
-    document.querySelector("h1").innerText = "Value is changed";
-  // console.log("Button is over :)");
-});
+// // mouseover
+// document.querySelector("[type='button']").addEventListener("mouseover",function() {
+//     console.log("Button is over :)");
+// });
 
-//mouseleave
 
-document.querySelector("[type='button']").addEventListener("mouseleave",function() {
-    document.querySelector("h1").innerText = "Updated Value";
-}); 
+// //whenever button is hover change the h1 content to "Value to change
+// document.querySelector("[type='button']").addEventListener("mouseover",function() {
+//     document.querySelector("h1").innerText = "Value is changed";
+//   // console.log("Button is over :)");
+// });
+
+// //mouseleave
+
+// document.querySelector("[type='button']").addEventListener("mouseleave",function() {
+//     document.querySelector("h1").innerText = "Updated Value";
+// }); 
 
