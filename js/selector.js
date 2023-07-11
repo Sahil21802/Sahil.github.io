612b2307e3a0ccfd2b195ed15ffa929aec8f423b
Sahil <ssharma438500>
2023-07-11 16:21:59 +0530

    add selector

---
+++ js/selector.js
@@ -0,0 +1,43 @@
+//selecting the element name 
+document.querySelector("nav")
+
+//selecting the element by class
+document.querySelector(".mobile")
+
+//selecting the element by id
+document.querySelector("#sidebar")
+
+//selecting multiple elements 
+document.querySelectorAll("div")
+
+// change the button color
+document.querySelector("[type = 'button']").style.background = "red";
+
+// change the content of element 
+document.querySelector("h1"). innerText = "Updated Value";
+
+
+// click
+document.querySelector("[type='button']").addEventListener("click",function() {
+    console.log("Button is clicked :)");
+});
+
+
+// mouseover
+document.querySelector("[type='button']").addEventListener("mouseover",function() {
+    console.log("Button is over :)");
+});
+
+
+//whenever button is hover change the h1 content to "Value to change
+document.querySelector("[type='button']").addEventListener("mouseover",function() {
+    document.querySelector("h1").innerText = "Value is changed";
+  // console.log("Button is over :)");
+});
+
+//mouseleave
+
+document.querySelector("[type='button']").addEventListener("mouseleave",function() {
+    document.querySelector("h1").innerText = "Updated Value";
+}); 
+
